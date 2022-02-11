0178316651581fff071b3059e0f5f8cc61299de7
ArsonistD <39225423+arsonistd@users.noreply.github.com>
2022-02-11 07:59:45 -0500

    Update Toolbar.lua

--- src/Plugin/Modules/Toolbar.lua
+++ src/Plugin/Modules/Toolbar.lua
@@ -8,8 +8,7 @@ local framework = require(script.Parent.Parent.ArsonsPluginFramework)
 
 local Toolbar = framework.CreateModule("Toolbar")
 
-Toolbar.toolbar = framework.plugin:CreateToolbar("LandscapeX")
-Toolbar.generateButton = Toolbar.toolbar:CreateButton("1", "Create and edit terrain", "rbxassetid://7588761139", "Generator")
-Toolbar.visualizeButton = Toolbar.toolbar:CreateButton("2", "Visualize terrain", "rbxassetid://7588761139", "Visualizer")
+Toolbar.toolbar = framework.plugin:CreateToolbar("Arson's Viewmodel Plugin")
+Toolbar.generateButton = Toolbar.toolbar:CreateButton("1", "Create and edit viewmodels", "rbxassetid://7588761139", "Create / Edit")
 
 return Toolbar
