684d3564765c4181e64ff774edc963a0a4bc3073
ArsonistD <39225423+arsonistd@users.noreply.github.com>
2022-02-11 09:15:41 -0500

    Update CameraPage.lua

--- src/Plugin/Components/Pages/CameraPage.lua
+++ src/Plugin/Components/Pages/CameraPage.lua
@@ -22,7 +22,38 @@ end
 function CameraPage:render()
 
     return Roact.createFragment({
-        
+        mainSettings =  Roact.createElement(self.Group, {
+            Text = "Main Generation Settings";
+            Locked = false;
+            LockedReason = "No biome is selected.";
+            Order = 100;
+            Contents = Roact.createFragment({
+                updateRealTimeBool = Roact.createElement(self.Setting , {
+                    Length = 30;
+                    Text = "Real Time Updates";
+                    Order = 1;
+                    Content = Roact.createElement(self.Bool, {
+                        Value = self.Generate.Settings.RealTimeUpdates;
+                    });
+                });
+                chunkDimension = Roact.createElement(self.Setting , {
+                    Length = 90;
+                    Text = "Chunk Dimensions";
+                    Order = 4;
+                    Content = Roact.createFragment({
+                        X = Roact.createElement(self.NumberBox, {
+                            YOffset = 30;
+                            Value = self.Generate.Settings.ChunkDimensions.X;
+                        });
+                        Y = Roact.createElement(self.NumberBox, {
+                            YOffset = 60;
+                            Value = self.Generate.Settings.ChunkDimensions.Y;
+                        });
+                    });
+                    ExtraText = {"X", "Y"}
+                });
+            });
+        });
     })
     
 end
